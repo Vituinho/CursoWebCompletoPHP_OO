1de89b6eabb274e3efc8e46028eec5cbeeaa6511
Vituinho <trabalhovictor531@gmail.com>
2025-08-22 10:58:27 -0300

    App Send Mail - Adicionando a biblioteca PHPMailer ao projeto

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -1,4 +1,18 @@
 <?php
+
+    require "./bibliotecas/src/Exception.php";
+    require "./bibliotecas/src/OAuthTokenProvider.php";
+    require "./bibliotecas/src/OAuth.php";
+    require "./bibliotecas/src/PHPMailer.php";
+    require "./bibliotecas/src/SMTP.php";
+    require "./bibliotecas/src/POP3.php";
+
+    
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
     //print_r($_POST);
 
     class Mensagem {
@@ -29,10 +43,47 @@
     $mensagem->__set('assunto', $_POST['assunto']);
     $mensagem->__set('mensagem', $_POST['mensagem']);
 
-    //print_r($mensagem);
+    print_r($mensagem);
 
-    if ($mensagem->mensagemValida()) {
-        echo 'Mensagem é valida';
-    } else {
+    if (!($mensagem->mensagemValida())) {
         echo 'Mensagem não é valida';
-    }
+        die();
+    } 
+
+    $mail = new PHPMailer(true);
+
+    try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.zoho.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'aprendiz370@gazin.net.br';                     //SMTP username
+    $mail->Password   = 'safadinho quer minha senha';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('aprendiz370@gazin.net.br', 'Vituinho Remetente');
+    $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $mensagem->__get('assunto');
+    $mail->Body    = $mensagem->__get('mensagem');
+    $mail->AltBody = 'É necessario que use um client que suporte HTML para ver essa mensagem';
+
+    $mail->send();
+    echo 'E-mail enviado com sucesso';
+} catch (Exception $e) {
+    echo 'Não foi possível enviar esse e-mail! Por favor tente novamente mais tarde <br>';
+    echo 'Detalhes do erro: ' . $mail->ErrorInfo;
+}
+    
